d2b6bf2a51d8091c72d03726d0f2ea98e454ef09
Muhammad Nur Yanhaona <mny9md@virginia.edu>
2015-10-17 10:25:44 -0400

    A work in progress commit for an MPI experiment on group-communicator setup

---
+++ Communication/mpi/segment_mpi_group.cpp
@@ -0,0 +1,63 @@
+#include <mpi.h>
+#include <vector>
+#include <iostream>
+#include "segment_mpi_group.h"
+
+using namespace std;
+
+SegmentGroup::SegmentGroup(vector<int> segments) {
+	this->segments = vector<int>(segments);
+	this->segmentRanks = vector<int>(segments);
+	mpiCommunicator = MPI_COMM_WORLD;
+}
+
+void SegmentGroup::setupCommunicator() {
+
+	int segmentRank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &segmentRank);
+
+	MPI_Group orig_group, new_group;
+	MPI_Comm_group(MPI_COMM_WORLD, &orig_group);
+
+	int participants = segments.size();
+	int *originalRanks = new int[participants];
+	for (unsigned int i = 0; i < segments.size(); i++) {
+		originalRanks[i] = segments.at(i);
+	}
+
+	MPI_Group_incl(orig_group, participants, originalRanks, &new_group);
+	MPI_Comm_create(MPI_COMM_WORLD, new_group, &mpiCommunicator);
+
+	int groupRank;
+	MPI_Comm_rank(mpiCommunicator, &groupRank);
+
+	int participantRanks[participants];
+	MPI_Allgather(&segmentRank, 1, MPI_INT, participantRanks, participants, MPI_INT, mpiCommunicator);
+
+	for (int rank = 0; rank < participants; rank++) {
+		int segmentTag = participantRanks[rank];
+		for (int i = 0; i < participants; i++) {
+			if (segments.at(i) == segmentTag) {
+				segmentRanks[i] = rank;
+				break;
+			}
+		}
+	}
+}
+
+int SegmentGroup::getRank(int segmentId) {
+	for (unsigned int i = 0; i < segments.size(); i++) {
+		if (segments.at(i) == segmentId) {
+			return segmentRanks.at(i);
+		}
+	}
+	return -1;
+}
+
+void SegmentGroup::describe(std::ostream &stream) {
+	stream << "Segment Group:\n";
+	for (unsigned int i = 0; i < segments.size(); i++) {
+		stream << '\t' << "Segment Id: " << segments.at(i) << ", ";
+		stream << "Group Rank:" << segmentRanks.at(i) << "\n";
+	}
+}
---
+++ Communication/test-cases/mpi_communicator_test.cpp
@@ -0,0 +1,39 @@
+#include <mpi.h>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include "../mpi/segment_mpi_group.h"
+
+using namespace std;
+
+#define NPROCS 8
+
+int main(int argc, char *argv[]) {
+
+	MPI_Init(&argc, &argv);
+
+	int rank;
+	int segmentCount;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &segmentCount);
+
+	int groupNo = rank % 3;
+	vector<int> segmentsInGroup;
+	for (int i = 0; i < segmentCount; i++) {
+		if (i % 3 == groupNo) segmentsInGroup.push_back(i);
+	}
+	SegmentGroup *group = new SegmentGroup(segmentsInGroup);
+	group->setupCommunicator();
+
+	ostringstream message;
+	message << "Process #" << rank << "'s ";
+	group->describe(message);
+	cout << message.str();
+	cout.flush();
+
+	MPI_Finalize();
+
+	return 0;
+}
