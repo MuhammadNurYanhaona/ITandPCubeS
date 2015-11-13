eb1fdb6fd94deb3f6808ff0f8cd6a7eab586b6af
Muhammad Nur Yanhaona <mny9md@virginia.edu>
2015-11-13 11:19:48 -0500

    updated PCubeS description of shen (in Rivana) and added two mapping files for it

--- SegmentedMemoryBackEnd/samples/pcubes/shen-cluster.ml
+++ SegmentedMemoryBackEnd/samples/pcubes/shen-cluster.ml
@@ -2,8 +2,7 @@
 //--------------------------------------------------------------------------------------------------------------------
 //Space #Number : 		$Space-Name	(#PPU-Count)	// Comments
 //--------------------------------------------------------------------------------------------------------------------
-Space	5:			Node		(1)		// cluster level
-Space 	4<unit><segment>:  	CPU 		(4)		// 4 CPUs (location of memory segmentation)
-Space	3:			Bi-Section	(2)		// 2 groups of cores in each CPU				
-Space 	2: 			Core-Group 	(10) 		// in each group, 25 MB L-3 cache for 10 cores		
-Space 	1<core>:		Core		(1)		// 256 KB L-2 cache for individual core
+Space	4:			Node		(1)		// cluster level
+Space 	3<unit><segment>:  	CPU 		(4)		// 4 CPUs (location of memory segmentation)
+Space	2:			Bi-Section	(2)		// 2 groups of 10 cores in each CPU, 25 MB L-3 cache per group				
+Space 	1<core>:		Core		(10)		// 256 KB L-2 cache for individual cores
