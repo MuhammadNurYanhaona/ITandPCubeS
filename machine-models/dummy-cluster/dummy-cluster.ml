add11e99980fb20e390dde6bd105e08d1ff693b9
Muhammad Nur Yanhaona <mny9md@virginia.edu>
2016-02-09 22:15:01 -0500

    added a dummy segmented-memory machine model and corresponding mapping files

---
+++ machine-models/dummy-cluster/dummy-cluster.ml
@@ -0,0 +1,7 @@
+//--------------------------------------------------------------------------------------------------------------------
+//Space #Number : 		$Space-Name	(#PPU-Count)	// Comments
+//--------------------------------------------------------------------------------------------------------------------
+Space	4:			Node		(1)		// cluster level
+Space 	3<unit><segment>:  	CPU 		(10)		// 10 CPUs
+Space	2:			Core		(2)		// 2 cores per CPU
+Space 	1<core>:		Hyperthread	(2)		// 2 hyperthreads per core
