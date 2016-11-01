4081250f739f9d91fe6652de87935742f71bd6ec
muhammadnuryanhaona <mny9md@virginia.edu>
2016-10-31 21:03:04 -0400

    added PCubeS files for the CPU cluster of Big Red II supercomputer

---
+++ compilers/segmented-memory-backend/samples/pcubes/bigred-host-cluster.ml
@@ -0,0 +1,10 @@
+//--------------------- PCubeS Description of the CPU cluster of the big Red II Supercomputer  -----------------------
+//--------------------------------------------------------------------------------------------------------------------
+//Space #Number : 		$Space-Name	(#PPU-Count)	// Comments
+//--------------------------------------------------------------------------------------------------------------------
+Space	6:			Cluster		(1)		// the whole cluster		
+Space   5<unit>:		Node		(344)		// multiple physical units at this level
+Space 	4<segment>:  		CPU 		(2)		// 31 GB RAM Per CPU (location of memory segmentation)						
+Space 	3: 			NUMA-Node 	(2) 		// 6 MB L-3 Cache		
+Space 	2: 			Core-Pair 	(4)		// 2 MB L-2 Cache (1 floating point unit per core-pair)
+Space 	1<core>:		Core		(2)		// 16 KB L-1 Cache (core numbering starts here)
