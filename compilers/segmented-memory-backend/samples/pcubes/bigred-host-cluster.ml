d7ff095f2446929e37aed0f8c187524f027106c3
muhammadnuryanhaona <mny9md@virginia.edu>
2016-10-31 22:08:35 -0400

    correction in a pcubes description

--- compilers/segmented-memory-backend/samples/pcubes/bigred-host-cluster.ml
+++ compilers/segmented-memory-backend/samples/pcubes/bigred-host-cluster.ml
@@ -3,8 +3,8 @@
 //Space #Number : 		$Space-Name	(#PPU-Count)	// Comments
 //--------------------------------------------------------------------------------------------------------------------
 Space	6:			Cluster		(1)		// the whole cluster		
-Space   5<unit>:		Node		(344)		// multiple physical units at this level
-Space 	4<segment>:  		CPU 		(2)		// 31 GB RAM Per CPU (location of memory segmentation)						
+Space   5<unit><segment>:	Node		(344)		// multiple physical units at this level
+Space 	4:  			CPU 		(2)		// 63 GB RAM for 2 CPUs						
 Space 	3: 			NUMA-Node 	(2) 		// 6 MB L-3 Cache		
 Space 	2: 			Core-Pair 	(4)		// 2 MB L-2 Cache (1 floating point unit per core-pair)
 Space 	1<core>:		Core		(2)		// 16 KB L-1 Cache (core numbering starts here)
